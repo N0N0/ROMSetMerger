c13be8746cc8c1d71692ad714479a235e8754b00
addi n/a <addi@example.com>
2016-05-28 13:26:37 +0000

    format

--- src/utils/XmlUtil.java
+++ src/utils/XmlUtil.java
@@ -6,7 +6,6 @@ import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
 import org.simpleframework.xml.stream.Format;
 
-import xml.Datafile;
 
 /**
  * Encapsulates methods that do XML stuff :)
